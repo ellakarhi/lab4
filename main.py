817ca0c330e0c4181b8b0d42e812b83a08905773
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-03-24 22:25:51 +0000

    the E and L were created for part 11

--- main.py
+++ main.py
@@ -15,12 +15,31 @@ def shortbox(x, y):
   t.fd(y)
   t.lt(90)
   t.fd(x)
+def shortbox_set():
+  t.rt(90)
+  t.fd(27)
+  t.rt(90)
 
+def l_setup():
+  t.lt(180)
+  t.penup()
+  t.fd(80)
+  t.pendown()
+  t.rt(90)
 
 t.lt(90)
 bigline(100, 20)
-t.backward(20)
+t.lt(180)
+t.fd(20)
 shortbox(40, 15)
+shortbox_set()
+shortbox(40, 15)
+shortbox_set()
+shortbox(40, 15)
+
+l_setup()
+
+bigline(100, 20)
 
 
 turtle.getscreen()._root.mainloop()
